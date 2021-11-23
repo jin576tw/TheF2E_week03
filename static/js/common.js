50c7878cb78f456c4704578c50a98b3b839db354
jin576tw <jin576tw@gmail.com>
2021-11-23 16:18:47 +0800

    first commit

---
+++ static/js/common.js
@@ -0,0 +1,20 @@
+// 使用api
+function getAuthorizationHeader() {
+  //  填入自己 ID、KEY 開始
+  let AppID = "2d9b9a569dab44dfbf33035ed2846ac6";
+  let AppKey = "0Ly8nNY_Mw4fL3dcFmKXx4y1mZo";
+  //  填入自己 ID、KEY 結束
+  let GMTString = new Date().toGMTString();
+  let ShaObj = new jsSHA("SHA-1", "TEXT");
+  ShaObj.setHMACKey(AppKey, "TEXT");
+  ShaObj.update("x-date: " + GMTString);
+  let HMAC = ShaObj.getHMAC("B64");
+  let Authorization =
+    'hmac username="' +
+    AppID +
+    '", algorithm="hmac-sha1", headers="x-date", signature="' +
+    HMAC +
+    '"';
+  return { Authorization: Authorization, "X-Date": GMTString };
+}
+
