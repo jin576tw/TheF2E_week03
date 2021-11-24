e550a416c7db8646ef25228dd139957afb04fd6e
jin576tw <jin576tw@gmail.com>
2021-11-24 16:54:10 +0800

    公車選單

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,3 +1,23 @@
 'use strict'
 
 
+
+$('.navbar_logo').click(function(){
+
+    $('#LandingPage').fadeIn(800)
+    $('#LandingPage').siblings('.Bus_page').fadeOut(1)
+
+})
+
+
+
+$('.navbarItem_warp li:nth-of-type(1),#BusStatus_btn').click(function(){
+
+    $('#BusStatusPage').fadeIn(800)
+    $('#BusStatusPage').siblings('.Bus_page').fadeOut(1)
+
+
+
+
+
+})
