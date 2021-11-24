e3cecb50cdb9e0d026b0c6357a6b6af9a91f1d46
jin576tw <jin576tw@gmail.com>
2021-11-24 18:06:34 +0800

    公車動態完善中

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -4,17 +4,28 @@
 
 $('.navbar_logo').click(function(){
 
-    $('#LandingPage').fadeIn(800)
-    $('#LandingPage').siblings('.Bus_page').fadeOut(1)
+    $('#LandingPage').fadeIn(800).siblings('.Bus_page').fadeOut(1)
+
+
+
+   
+
+    $('.Navbar').removeClass('Navbar_grey')
+    $('.navbar_page,.Route_SearchWarp').hide()
 
 })
 
 
 
-$('.navbarItem_warp li:nth-of-type(1),#BusStatus_btn').click(function(){
+$('.navbarItem_warp li:nth-of-type(1),#BusName_btn').click(function(){
 
-    $('#BusStatusPage').fadeIn(800)
-    $('#BusStatusPage').siblings('.Bus_page').fadeOut(1)
+    $('#BusStatusPage').fadeIn(800).siblings('.Bus_page').fadeOut(1)
+
+
+    
+    $('.Navbar').addClass('Navbar_grey')
+
+    $('.navbar_page,.Route_SearchWarp').show()
 
 
 
